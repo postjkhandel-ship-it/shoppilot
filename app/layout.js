05c4482b585cc7f1653114a93bd5797ed2561dc0
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-05-19 17:39:46 +0200

    Delete app directory

--- app/layout.js
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="da">
-      <body>{children}</body>
-    </html>
-  );
-}
--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "50px", fontFamily: "Arial" }}>
-      <h1>Shoppilot virker nu</h1>
-      <p>Din Shopify-lignende platform er live.</p>
-    </main>
-  );
-}
