b600e8b3045c852148165ce4874c9e4fc4ad4ed8
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-05-20 17:42:21 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -38,6 +38,7 @@ export default function RootLayout({ children }) {
               <a style={link} href="/plans">Abonnementer</a>
               <a style={link} href="/notifications">Notifikationer</a>
               <a style={link} href="/support">Support</a>
+              <a style={link} href="/account">Min konto</a>
               <a style={link} href="/settings">Indstillinger</a>
               <a style={link} href="/store">Webshop Preview</a>
             </nav>
